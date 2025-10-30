10b8071810b0f03e8493f85a22b23dfb161383a9
khyaejin <olivia019182@naver.com>
2025-10-31 07:16:49 +0900

    fix(entity): PerformanceMoreLink가 BaseEntity를 상속하도록 수정

--- src/main/java/com/deulbull/performance/domain/performance/entity/PerformanceMoreLink.java
+++ src/main/java/com/deulbull/performance/domain/performance/entity/PerformanceMoreLink.java
@@ -1,6 +1,7 @@
 package com.deulbull.performance.domain.performance.entity;
 
 import com.deulbull.performance.domain.performance.entity.enums.LinkType;
+import com.deulbull.performance.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -10,7 +11,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class PerformanceMoreLink {
+public class PerformanceMoreLink extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
