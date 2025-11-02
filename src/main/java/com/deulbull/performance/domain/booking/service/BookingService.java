b7f7e3971b1a8d1ebb3178ef8c813c36fe7bfb8f
donggeon <dkms625@naver.com>
2025-11-02 14:50:46 +0900

    feat(booking): 공연 문의링크 조회 서비스 구현

--- src/main/java/com/deulbull/performance/domain/booking/service/BookingService.java
+++ src/main/java/com/deulbull/performance/domain/booking/service/BookingService.java
@@ -1,8 +1,12 @@
 package com.deulbull.performance.domain.booking.service;
 
 import com.deulbull.performance.domain.booking.web.dto.BookingRequestDto;
+import com.deulbull.performance.domain.booking.web.dto.OpenChatUrlResponse;
 
 public interface BookingService {
     // 예매 생성
     void createBooking(Long performanceId, BookingRequestDto requestDto);
+
+    // 공연 문의링크 조회
+    OpenChatUrlResponse getOpenChatUrl(Long performanceId);
 }
