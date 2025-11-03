4c78c48fc381f8cae3f349824e6d94f08caf9d0b
jhkim <juho1589@gmail.com>
2025-11-04 00:09:08 +0900

    feat(Dto): PerformanceSongsDetailResponseDto record 생성
    - Track(곡의 세부 정보를 담고 있음), Team(곡에 참여하는 인원의 정보)를 보내주는 record

---
+++ src/main/java/com/deulbull/performance/domain/performanceSongs/web/dto/PerformanceSongsDetailResponseDto.java
@@ -0,0 +1,28 @@
+package com.deulbull.performance.domain.performanceSongs.web.dto;
+
+import com.deulbull.performance.domain.band.entity.enums.SessionType;
+
+import java.util.List;
+
+public record PerformanceSongsDetailResponseDto (
+        Track track,
+        List<Team> team
+) {
+    public record Track(
+            int likes,
+            String title,
+            String artist,
+            String album,
+            String genre,
+            String releaseDate,
+            String youtubeUrl,
+            String lyrics,
+            String albumImgUrl
+    ) {}
+
+    public record Team(
+            SessionType session,
+            String name,
+            String instagramId
+    ) {}
+}
