21191fcf8e55a2b1a34239a65ad7132e028880d0
jhkim <juho1589@gmail.com>
2025-11-05 16:55:35 +0900

    fix(Controller): PerformanceSongsControllerController 리턴 방식 수정
    - 기존: PerformanceSongsDetailsResponseDto -> 변경: SuccessResponse
    - RequestMapping에서 /api구문 삭제

--- src/main/java/com/deulbull/performance/domain/performanceSongs/web/controller/PerformanceSongsController.java
+++ src/main/java/com/deulbull/performance/domain/performanceSongs/web/controller/PerformanceSongsController.java
@@ -2,6 +2,7 @@ package com.deulbull.performance.domain.performanceSongs.web.controller;
 
 import com.deulbull.performance.domain.performanceSongs.service.PerformanceSongsService;
 import com.deulbull.performance.domain.performanceSongs.web.dto.PerformanceSongsDetailResponseDto;
+import com.deulbull.performance.global.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,13 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/tracks")
+@RequestMapping("/tracks")
 @RequiredArgsConstructor
 public class PerformanceSongsController {
 
     private final PerformanceSongsService performanceSongsService;
     @GetMapping("/{performanceSongId}")
-    public PerformanceSongsDetailResponseDto getPerformanceSongDetails(@PathVariable Long performanceSongId) {
-        return performanceSongsService.getPerformanceSongsDetail(performanceSongId);
+    public SuccessResponse<PerformanceSongsDetailResponseDto> getPerformanceSongDetails(@PathVariable Long performanceSongId) {
+        return SuccessResponse.ok(performanceSongsService.getPerformanceSongsDetail(performanceSongId));
     }
 }
