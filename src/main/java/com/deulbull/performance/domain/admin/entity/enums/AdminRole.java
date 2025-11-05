3b0a71b1407ce9dd70e6779168fa87c73042bf51
khyaejin <olivia019182@naver.com>
2025-11-05 19:56:10 +0900

    fix(entity): AdminRole에 MASTER 권한 추가

--- src/main/java/com/deulbull/performance/domain/admin/entity/enums/AdminRole.java
+++ src/main/java/com/deulbull/performance/domain/admin/entity/enums/AdminRole.java
@@ -3,5 +3,6 @@ package com.deulbull.performance.domain.admin.entity.enums;
 public enum AdminRole {
     VIEWER, // 보기만 가능
     EDITOR, // 수정 가능
-    ADMIN // 최상위 관리자
+    ADMIN, // 동아리별 최상위 관리자
+    MASTER // 시스템 전체 최상위 관리자
 }
