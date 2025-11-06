63dbe8a911038cdc06011e8143c88b6a1cd07109
jhkim <juho1589@gmail.com>
2025-11-06 15:53:26 +0900

    feat(Controller): getlike 생성
    - getlike는 인자로 Id와 Dto를 받으며 이때 유효성 검사를 통해 Param이 왔는지 확인한다.

--- src/main/java/com/deulbull/performance/domain/performanceSongs/web/controller/PerformanceSongsController.java
+++ src/main/java/com/deulbull/performance/domain/performanceSongs/web/controller/PerformanceSongsController.java
@@ -2,12 +2,12 @@ package com.deulbull.performance.domain.performanceSongs.web.controller;
 
 import com.deulbull.performance.domain.performanceSongs.service.PerformanceSongsService;
 import com.deulbull.performance.domain.performanceSongs.web.dto.PerformanceSongsDetailResponseDto;
+import com.deulbull.performance.domain.performanceSongs.web.dto.PerformanceSongsLikeRequestDto;
+import com.deulbull.performance.domain.performanceSongs.web.dto.PerformanceSongsLikeResponseDto;
 import com.deulbull.performance.global.response.SuccessResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tracks")
@@ -15,8 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class PerformanceSongsController {
 
     private final PerformanceSongsService performanceSongsService;
+
     @GetMapping("/{performanceSongId}")
     public SuccessResponse<PerformanceSongsDetailResponseDto> getPerformanceSongDetails(@PathVariable Long performanceSongId) {
         return SuccessResponse.ok(performanceSongsService.getPerformanceSongsDetail(performanceSongId));
+    }
+
+    @PostMapping("/{performanceSongId}/like")
+    public SuccessResponse<PerformanceSongsLikeResponseDto> getlike(@PathVariable Long performanceSongId, @Valid @RequestBody PerformanceSongsLikeRequestDto liked) {
+        return SuccessResponse.ok(performanceSongsService.getPerformanceSongsLike(performanceSongId, liked));
     }
 }
