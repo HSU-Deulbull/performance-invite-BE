5346145f1a1669e3205ee2fa47f8387eb34d3b8b
jhkim <juho1589@gmail.com>
2025-11-06 15:48:44 +0900

    feat(Repository): likes, findLikesById 생성
    - likes는 해당 곡의 좋아요의 수를 증가/감소 시키기 위해 Update하는 기능을 한다.
    - findLikesById는 id를 통해 likes의 수를 가져오는 기능을 한다.

--- src/main/java/com/deulbull/performance/domain/performanceSongs/repository/PerformanceSongsRepository.java
+++ src/main/java/com/deulbull/performance/domain/performanceSongs/repository/PerformanceSongsRepository.java
@@ -2,6 +2,9 @@ package com.deulbull.performance.domain.performanceSongs.repository;
 
 import com.deulbull.performance.domain.performanceSongs.entity.PerformanceSong;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +12,9 @@ import java.util.Optional;
 @Repository
 public interface PerformanceSongsRepository extends JpaRepository<PerformanceSong, Long> {
     Optional<PerformanceSong> findWithSongById(Long id);
+    @Modifying
+    @Query(value = "update performance_song set likes = greatest(likes+:delta, 0) where id = :id", nativeQuery = true)
+    int likes(@Param("id") Long id, @Param("delta") int delta);
+
+    Optional<LikesOnly> findLikesById(Long performanceSongId);
 }
