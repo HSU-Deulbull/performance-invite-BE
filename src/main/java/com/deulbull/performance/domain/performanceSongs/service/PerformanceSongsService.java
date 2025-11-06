80f083c7fca28c294707d5cd1d6372ca02f758e9
jhkim <juho1589@gmail.com>
2025-11-06 15:52:22 +0900

    feat(Service): getPerformanceSongsLike 생성
    - getPerformanceSongsLike는 현재 곡의 좋아요 개수를 변경시키는 Service 함수이다.

--- src/main/java/com/deulbull/performance/domain/performanceSongs/service/PerformanceSongsService.java
+++ src/main/java/com/deulbull/performance/domain/performanceSongs/service/PerformanceSongsService.java
@@ -1,8 +1,12 @@
 package com.deulbull.performance.domain.performanceSongs.service;
 
 import com.deulbull.performance.domain.performanceSongs.web.dto.PerformanceSongsDetailResponseDto;
+import com.deulbull.performance.domain.performanceSongs.web.dto.PerformanceSongsLikeRequestDto;
+import com.deulbull.performance.domain.performanceSongs.web.dto.PerformanceSongsLikeResponseDto;
 
 public interface PerformanceSongsService {
     // 곡 정보 상세 조회
     PerformanceSongsDetailResponseDto getPerformanceSongsDetail(Long performanceSongId);
+    // 곡 좋아요 추가/취소
+    PerformanceSongsLikeResponseDto getPerformanceSongsLike(Long performanceSongId, PerformanceSongsLikeRequestDto liked);
 }
