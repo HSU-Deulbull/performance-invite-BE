f18939083dda6af65d2b9cb88ca2bc9251d4af3a
jhkim <juho1589@gmail.com>
2025-11-08 17:48:39 +0900

    feat(Service): AdminPerformanceService 업데이트
    - 다음 곡 / 이전 곡을 전달하고 id로 Projection을 생성하는 코드 작성

--- src/main/java/com/deulbull/performance/domain/admin/service/AdminPerformanceService.java
+++ src/main/java/com/deulbull/performance/domain/admin/service/AdminPerformanceService.java
@@ -3,5 +3,12 @@ package com.deulbull.performance.domain.admin.service;
 import com.deulbull.performance.domain.admin.web.dto.AdminCurrentSongResponseDto;
 
 public interface AdminPerformanceService {
+    // 현재 곡 조회
     AdminCurrentSongResponseDto getCurrentSong(Long adminId);
+
+    // 다음 곡 전달
+    AdminCurrentSongResponseDto getNextSong(Long adminId);
+
+    // 이전 곡 전달
+    AdminCurrentSongResponseDto getPreviousSong(Long adminId);
 }
