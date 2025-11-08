d1b9898dcab750ebb342889cb23ca822ed56eab1
jhkim <juho1589@gmail.com>
2025-11-08 17:48:12 +0900

    feat(Repository): PerformanceSongsRepository 업데이트
    - 다음 곡 / 이전 곡을 전달하고 id로 Projection을 생성하는 코드 작성

--- src/main/java/com/deulbull/performance/domain/performanceSongs/repository/PerformanceSongsRepository.java
+++ src/main/java/com/deulbull/performance/domain/performanceSongs/repository/PerformanceSongsRepository.java
@@ -1,6 +1,7 @@
 package com.deulbull.performance.domain.performanceSongs.repository;
 
 import com.deulbull.performance.domain.performanceSongs.entity.PerformanceSong;
+import com.deulbull.performance.domain.song.projection.CurrentSongProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -17,4 +18,31 @@ public interface PerformanceSongsRepository extends JpaRepository<PerformanceSon
     int likes(@Param("id") Long id, @Param("delta") int delta);
 
     Optional<LikesOnly> findLikesById(Long performanceSongId);
+
+    // 다음 곡: current.orderInPerformance보다 큰 곡 중 가장 작은 순서
+    Optional<PerformanceSong> findFirstByPerformance_IdAndOrderInPerformanceGreaterThanOrderByOrderInPerformanceAsc(
+            Long performanceId, Integer orderInPerformance
+    );
+
+    // 첫 곡: 현재곡이 없거나 마지막에서 다음이 없을 때
+    Optional<PerformanceSong> findFirstByPerformance_IdOrderByOrderInPerformanceAsc(Long performanceId);
+
+    // 이전 곡: 현재 순번보다 작은 것 중 가장 큰 순번
+    Optional<PerformanceSong>
+    findFirstByPerformance_IdAndOrderInPerformanceLessThanOrderByOrderInPerformanceDesc(
+            Long performanceId, Integer orderInPerformance
+    );
+
+    // 마지막 곡
+    Optional<PerformanceSong>
+    findFirstByPerformance_IdOrderByOrderInPerformanceDesc(Long performanceId);
+
+    // ps.id로 프로젝션 1건 찾기
+    @Query("""
+        select s.title as title, s.artist as artist, s.albumImgUrl as albumImgUrl
+        from PerformanceSong ps
+        join ps.song s
+        where ps.id = :performanceSongId
+    """)
+    Optional<CurrentSongProjection> findProjectionById(@Param("performanceSongId") Long performanceSongId);
 }
