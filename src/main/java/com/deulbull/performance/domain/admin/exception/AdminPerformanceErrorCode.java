efd946b7495c697a4bed96bb8e4f2ad2aee8bfc2
jhkim <juho1589@gmail.com>
2025-11-19 18:24:13 +0900

    feat(Exception): 다음, 이전 곡 에러 핸들링 생성
    - 다음 곡 넘기기 404, 이전 곡 돌아가기 404 추가

--- src/main/java/com/deulbull/performance/domain/admin/exception/AdminPerformanceErrorCode.java
+++ src/main/java/com/deulbull/performance/domain/admin/exception/AdminPerformanceErrorCode.java
@@ -12,7 +12,10 @@ import static com.deulbull.performance.global.constant.StaticValue.NO_CONTENT;
 public enum AdminPerformanceErrorCode implements BaseResponseCode {
     ADMIN_SONG_404_NOT_FOUND("ADMIN_SONG_404_NOT_FOUND", NOT_FOUND, "해당 공연에 등록된 곡이 없습니다."),
     ADMIN_PERFORMANCE_404_NOT_FOUND("ADMIN_PERFORMANCE_404_NOT_FOUND", NOT_FOUND, "해당 공연의 ID가 없습니다."),
+    PERFORMANCE_NEXT_SONG_404_NOT_FOUND("PERFORMANCE_NEXT_SONG_404_NOT_FOUND", NOT_FOUND, "해당 곡이 마지막 곡입니다."),
+    PERFORMANCE_PREVIOUS_SONG_404_NOT_FOUND("PERFORMANCE_PREVIOUS_SONG_404_NOT_FOUND", NOT_FOUND, "이전 곡을 찾을 수 없습니다."),
     PERFORMANCE_SONG_204_NO_CONTENT("PERFORMANCE_SONG_204_NO_CONTENT", NO_CONTENT,"아직 공연 시작 전 입니다.");
+
     private final String code;
     private final int httpStatus;
     private final String message;
